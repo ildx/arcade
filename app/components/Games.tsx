7cbffac6ff6ec623cb4ade41aacc540b4789b01c
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-01-28 18:16:08 +0200

    hide table cells on mobile

--- app/components/Games.tsx
+++ app/components/Games.tsx
@@ -30,8 +30,10 @@ export const Games = () => {
                 />
               </div>
             </Table.Heading>
-            <Table.Heading width="w-[30%]">Console</Table.Heading>
-            <Table.Heading alignCenter width="w-[15%]">
+            <Table.Heading responsive width="w-[30%]">
+              Console
+            </Table.Heading>
+            <Table.Heading responsive alignCenter width="w-[15%]">
               On a wishlist?
             </Table.Heading>
           </tr>
@@ -50,8 +52,8 @@ export const Games = () => {
                 className={clsx(index % 2 !== 0 && 'bg-arc-blue-dark')}
               >
                 <Table.Cell>{game.name}</Table.Cell>
-                <Table.Cell>{game.platform.console}</Table.Cell>
-                <Table.Cell alignCenter>
+                <Table.Cell responsive>{game.platform.console}</Table.Cell>
+                <Table.Cell responsive alignCenter>
                   <div className="m-auto max-w-3">
                     {!game.owner && <Icon.Close color="fill-arc-beige" />}
                   </div>
@@ -69,14 +71,22 @@ interface IHeadingProps {
   alignCenter?: boolean;
   children: React.ReactNode;
   onClick?: () => void;
+  responsive?: boolean;
   width: string;
 }
 
-const Heading = ({ alignCenter, children, onClick, width }: IHeadingProps) => (
+const Heading = ({
+  alignCenter,
+  children,
+  onClick,
+  responsive,
+  width,
+}: IHeadingProps) => (
   <th
     className={clsx(
       width,
       alignCenter && 'text-center',
+      responsive && 'hidden sm:table-cell',
       'border-b border-arc-beige px-4 py-2 text-left text-arc-purple-hilight',
     )}
     onClick={onClick}
@@ -88,10 +98,17 @@ const Heading = ({ alignCenter, children, onClick, width }: IHeadingProps) => (
 interface ICellProps {
   alignCenter?: boolean;
   children: React.ReactNode;
+  responsive?: boolean;
 }
 
-const Cell = ({ alignCenter, children }: ICellProps) => (
-  <td className={clsx('px-4 py-2', alignCenter && 'text-center')}>
+const Cell = ({ alignCenter, children, responsive }: ICellProps) => (
+  <td
+    className={clsx(
+      'px-4 py-2',
+      alignCenter && 'text-center',
+      responsive && 'hidden sm:table-cell',
+    )}
+  >
     {children}
   </td>
 );
