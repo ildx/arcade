e9e618d1526e172cb64a63dc3da28719427b7e2d
Iivari Leinonen <iivari.leinonen@gmail.com>
2024-03-26 14:36:02 +0200

    update footer copyright

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => (
   <footer className="w-full">
     <section className="m-auto max-w-4xl px-4 pb-8 pt-4 text-center">
-      <span>0x6969766f &copy; {new Date().getFullYear()}</span>
+      <span>ildx &copy; {new Date().getFullYear()}</span>
     </section>
   </footer>
 );
